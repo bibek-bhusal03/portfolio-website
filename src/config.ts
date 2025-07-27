8e86c0ddc12c5d0e5d0bcd822183eaac79541aa7
bibek-bhusal03 <bibekbhusal455@gmail.com>
2025-07-27 12:05:09 +0545

    update font size of site title

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const SITE: Site = {
   ogImage: "ogImage.png",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 6,
+  postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
